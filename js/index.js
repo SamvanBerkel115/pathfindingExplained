c563f5dba934888e852b50b1c798a6fab0e9f174
SamvanBerkel115 <36756835+SamvanBerkel115@users.noreply.github.com>
2019-10-16 12:20:19 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,31 @@
+var pathfinding = {
+    set: {
+        grid: function() {
+            const cellWidth = 30;
 
+            const gridDiv = document.getElementById('grid');
+        
+            
+            const gridWidth = gridDiv.clientWidth;
+            const gridHeight = gridDiv.clientHeight;
+
+            const amountOfColumns = gridWidth / cellWidth;
+            const amountOfRows = gridHeight / cellWidth;
+
+            gridDiv.style.gridTemplateColumns = `repeat(${cellWidth}px, ${amountOfColumns})`;
+            gridDiv.style.gridTemplateRows = `repeat(${cellWidth}px, ${amountOfRows})`;
+
+            for (let c = 0; c < amountOfColumns; c++) {
+                for (let r = 0; r < amountOfRows; r++) {
+                    const cellDiv = document.createElement('div');
+                    cellDiv.classList.add('gridCell');
+                }
+            }
+        }
+    },
+    init: function() {
+        pathfinding.set.grid();
+    }
+}
+
+window.onload = pathfinding.init();
